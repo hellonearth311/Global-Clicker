7ca08f4d23d95b34037dcb718244138acb01a55c
swaritnarang <narangswarit02@gmail.com>
2026-06-25 17:09:57 -0500

    click model, user ip, clicking the button does something now

--- global_clicker_app/models.py
+++ global_clicker_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Click(models.Model):
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Click from {self.ip_address} at {self.timestamp}"
--- global_clicker_app/views.py
+++ global_clicker_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
+from ipware import get_client_ip
+
+def get_user_ip(request):
+    client_ip, is_routable = get_client_ip(request)
+    if client_ip:
+        return client_ip
 
 def redirect_to_clicker(request):
     return redirect("/clicker/")
 
 def clicker(request):
     clicks = 0 # fetch em later with like Click.objects.all() or something
+    ip = get_user_ip(request)
 
     return render(request, "global_clicker_app/templates/clicker.html", {
-        "clicks": clicks
+        "clicks": clicks,
+        "ip": ip
     })
