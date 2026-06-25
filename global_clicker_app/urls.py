1a586105f111b3e2db6a9e55522d2c264b373c49
swaritnarang <narangswarit02@gmail.com>
2026-06-24 23:25:12 -0500

    add some more content, clicking doesn't work yet. no map yet either

--- global_clicker_app/urls.py
+++ global_clicker_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from global_clicker_app import views
 
 urlpatterns = [
-    path("clicker/", views.clicker, name="clicker")
+    path("clicker/", views.clicker, name="clicker"),
+    path("", views.redirect_to_clicker, name="redirect"),
 ]
--- global_clicker_app/views.py
+++ global_clicker_app/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def redirect_to_clicker(request):
+    return redirect("/clicker/")
 
 def clicker(request):
-    return render(request, "global_clicker_app/templates/clicker.html")
+    clicks = 0 # fetch em later with like Click.objects.all() or something
+
+    return render(request, "global_clicker_app/templates/clicker.html", {
+        "clicks": clicks
+    })
