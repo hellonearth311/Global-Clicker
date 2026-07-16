372bde06f32dbbc55330d5af55e12bf38eb37f88
swaritnarang <narangswarit02@gmail.com>
2026-07-16 01:56:17 -0500

    no js required!

--- global_clicker_app/static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    console.log("clicked");
-    document.getElementById("clicker").click();
-});
-
-function handleClick() {
-    console.log("clicked");
-};
